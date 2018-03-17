af5b76b61cb6512e6072218093422d005eabc1e6
MelissaVines <36156887+MelissaVines@users.noreply.github.com>
2018-03-17 20:24:28 +0000

    Made new repository, added Unity file

---
+++ Hay_Wired/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string logText="";
+	public float speed = 2;
+	public float jumpSpeed = 5;
+	public float health = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+		float horizontal = Input.GetAxis ("Horizontal");
+		bool jump = Input.GetButtonDown ("Jump");
+		Collider2D collider = GetComponent<Collider2D>();
+		LayerMask groundLayer = LayerMask.GetMask ("Ground");
+		bool touchingGround = collider.IsTouchingLayers (groundLayer);
+		Vector2 velocity = rigidBody.velocity;
+		velocity.x = horizontal * speed;
+		if (jump == true && touchingGround == true) {
+			velocity.y = jumpSpeed;
+		}
+		rigidBody.velocity = velocity;
+	}
+
+	public void ApplyDamage (float damageDealt){
+		health = health - damageDealt;
+	}
+}
